0231a9bfb5c94ab7fa2440395fcbd1277d0b5d78
John Cui <jiacui@cisco.com>
2024-08-06 04:35:04 +0800

    correct

--- setup.sh
+++ setup.sh
@@ -1,30 +1,32 @@
-mkdir ~/setup
-# move setup yaml to ~/setup
+#!/bin/bash
+
+# Create setup directory and move setup yaml
+mkdir -p ~/setup
 cd ~/setup
+
+# Clone the hands-on lab setup repository
 git clone https://github.com/johncui0505/hands-on_lab_setup
 
 cd hands-on_lab_setup
 
-# ACI ansible module installation
+# Install ACI Ansible modules
 ansible-galaxy collection install cisco.aci
-
 ansible-galaxy collection install cisco.nd
 ansible-galaxy collection install community.general
 
-#initial setup ACI fabric
+# Initial setup of ACI fabric
 # Leaf1 NodeID: 101 Serial: TEP-1-101
 # Leaf2 NodeID: 102 Serial: TEP-1-102
 # Spine1 NodeID: 201 Serial: TEP-1-103
 # Spine2 NodeID: 202 Serial: TEP-1-104
 # NTP 198.18.128.1
-
 ansible-playbook aci_fabric_setup.yml
 
 # ACI fabric initial setup done
 
-# create workfolder
-mkdir ~/labs
+# Create work folder and clone ansible terraform demo repository
+mkdir -p ~/labs
 cd ~/labs
 git clone https://github.com/johncui0505/ansible_terraform_demo
 
-cd ~/labs/ansible_terraform_demo
+
